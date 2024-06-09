09f469133d00de64acc8b12ea76280fd0e4683e6
ManjirouSano69 <168046394+ManjirouSano69@users.noreply.github.com>
2024-06-09 11:58:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ from os import getenv
 from dotenv import load_dotenv
 load_dotenv()
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "22867431"))
+API_HASH = getenv("API_HASH", "24ef0e76ceb137563dc33722e4cd79bd")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-LOG_ID = int(getenv("LOG_ID", ""))
+BOT_TOKEN = getenv("BOT_TOKEN", "7483130894:AAH9VSFstcInQmPS2NGEKv5ivcq_XtAfkx8")
+LOG_ID = int(getenv("LOG_ID", "-1002197525160"))
 
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
-SUDO_USER = list(map(int, getenv("SUDO_USER", "").split()))
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://niksharma92297:afk@cluster0.wj4ftow.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+SUDO_USER = list(map(int, getenv("SUDO_USER", "1993048420 5743248220").split()))
